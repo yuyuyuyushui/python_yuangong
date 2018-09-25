133283a5652f88625203a7cbc68c09dcf3a8d7d3
Administrator <786313105@qq.com>
2018-09-25 15:22:27 +0800

    员工信息表

--- test3.py
+++ test3.py
@@ -1,4 +1,11 @@
-a = 'select r from db where a > 4'
-x,y = a.split('where')
-print(repr(x.strip()), y.strip())
-select name from db where age > 24
+a = [1,2,3,4]
+data = input('>>>')
+new = 'jk'
+if int(data) in a:
+    print("ok")
+else:
+    print("lose")
+    new = 3
+if new == 3:
+    print('yes')
+select age,name from db where age > 24
